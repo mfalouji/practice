d1f4b6c87768b25ad01e8b37a5cf4064d5fd6609
Mahdi Falouji <mfalouji@umich.edu>
2024-04-23 14:07:25 -0400

    updated code

--- src/SelectionSortTest.java
+++ src/SelectionSortTest.java
@@ -6,27 +6,26 @@ public class SelectionSortTest {
 
 	@Test
     public void testPositive() {
-        SelectionSort sort = new SelectionSort();
-        int[] actual = {5, 1, 4, 2, 8};
-        int[] expected = {1, 2, 4, 5, 8};
-        sort.selectionSort(actual);
-        assertArrayEquals(expected, actual);
+		SelectionSort sort = new SelectionSort();
+	    int[] actual = {34, 8, 64, 51, 32, 21};
+	    int[] expected = {8, 21, 32, 34, 51, 64};
+	    sort.selectionSort(actual);
+	    assertArrayEquals(expected, actual);
     }
-
     @Test
     public void testNegatives() {
-        SelectionSort sort = new SelectionSort();
-        int[] actual = {-1, -3, -4, -2, -5};
-        int[] expected = {-5, -4, -3, -2, -1};
+    	SelectionSort sort = new SelectionSort();
+        int[] actual = {-7, -15, -3, -100, -22};
+        int[] expected = {-100, -22, -15, -7, -3};
         sort.selectionSort(actual);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void testMixed() {
-        SelectionSort sort = new SelectionSort();
-        int[] actual = {0, -1, 5, -3, 2};
-        int[] expected = {-3, -1, 0, 2, 5};
+    	SelectionSort sort = new SelectionSort();
+        int[] actual = {0, 5, -15, 7, -3, 2};
+        int[] expected = {-15, -3, 0, 2, 5, 7};
         sort.selectionSort(actual);
         assertArrayEquals(expected, actual);
     }
